7011b1e3c356462f71b76d9affaeafdd3de7c351
nicholaskchoi <nick.k.choi@berkeley.edu>
2016-11-29 02:09:35 +0000

    modified the shop page: thanks susie for doing most of the work!

--- app/assets/javascripts/components/shop_app.js.jsx
+++ app/assets/javascripts/components/shop_app.js.jsx
@@ -1,10 +1,56 @@
 var ShopApp = React.createClass({
 
   render: function() {
+    
     return (
       <div className='shopApp-container'>
-        <div className='shopApp-header'> <h3> Shop App Component </h3> </div>
-        <div className='shopApp-body'> <h3> The App Body </h3> </div>
+        <div className='shopApp-header'> 
+          <div className="navigation">
+                <ul>
+                  <a href="/shop">Originals</a>
+                  <a href="/shop">Women</a>
+                  <a href="/shop">Men</a>
+                  <a href="/shop">About</a>
+                </ul>
+            </div>
+          <h3> 'CLOTHES CATEGORY NAME' </h3> 
+        </div>
+        <div className='item_container'>
+          <div className='item_desc'> <p> Item Name; $100</p> </div>
+          <img src="http://www.myfamily.it/content/images/thumbs/0024087.jpeg"></img>
+        </div>
+        <div className='item_container'>
+          <div className='item_desc'> <p> Item Name; $100</p> </div>
+          <img src="http://www.myfamily.it/content/images/thumbs/0024087.jpeg"></img>
+        </div>
+        <div className='item_container'>
+          <div className='item_desc'> <p> Item Name; $100</p> </div>
+          <img src="http://www.myfamily.it/content/images/thumbs/0024087.jpeg"></img>
+        </div>
+        <div className='item_container'>
+          <div className='item_desc'> <p> Item Name; $100</p> </div>
+          <img src="http://www.myfamily.it/content/images/thumbs/0024087.jpeg"></img>
+        </div>
+        <div className='item_container'>
+          <div className='item_desc'> <p> Item Name; $100</p> </div>
+          <img src="http://www.myfamily.it/content/images/thumbs/0024087.jpeg"></img>
+        </div>
+        <div className='item_container'>
+          <div className='item_desc'> <p> Item Name; $100</p> </div>
+          <img src="http://www.myfamily.it/content/images/thumbs/0024087.jpeg"></img>
+        </div>
+        <div className='item_container'>
+          <div className='item_desc'> <p> Item Name; $100</p> </div>
+          <img src="http://www.myfamily.it/content/images/thumbs/0024087.jpeg"></img>
+        </div>
+        <div className='item_container'>
+          <div className='item_desc'> <p> Item Name; $100</p> </div>
+          <img src="http://www.myfamily.it/content/images/thumbs/0024087.jpeg"></img>
+        </div>
+        <div className='item_container'>
+          <div className='item_desc'> <p> Item Name; $100</p> </div>
+          <img src="http://www.myfamily.it/content/images/thumbs/0024087.jpeg"></img>
+        </div>
       </div>
     );
   }
