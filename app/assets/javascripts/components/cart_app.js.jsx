f18ac7d2e423e0d6d4951043de2e858090589838
Andrew Chang <andrewc@calvisitor-10-105-183-80.calvisitor.1918.berkeley.edu>
2016-11-28 18:14:15 -0800

    added files to setup cart page

---
+++ app/assets/javascripts/components/cart_app.js.jsx
@@ -0,0 +1,11 @@
+var CartApp = React.createClass({
+
+  render: function() {
+    return (
+      <div className='cartApp-container'>
+        <div className='cartApp-header'> <h3> Cart App </h3> </div>
+        <div className='cartApp-body'> <h3> Test </h3> </div>
+      </div>
+    );
+  }
+});
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   get 'shop' => 'shop#pagecontainer'
   
-  match '/cart' => 'pages#cart'
+  get 'cart' => 'shop#cart'
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
