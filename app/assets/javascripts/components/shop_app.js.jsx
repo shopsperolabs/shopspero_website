6f651ac60c2a2e4b090b498f86550d06b478178d
nicholaskchoi <nick.k.choi@berkeley.edu>
2016-12-29 00:55:04 +0000

    got the shop page to look servicable

--- app/assets/javascripts/components/shop_app.js.jsx
+++ app/assets/javascripts/components/shop_app.js.jsx
@@ -1,7 +1,19 @@
 var ShopApp = React.createClass({
-
+  
+  propTypes: {
+    title: React.PropTypes.number,
+  },
+  
+  getInitialState: function() {
+    return { items: [{id: 1}, {id: 2}, {id:3}, {id:4}, {id:5}, {id:6}] };
+  },
+  
+  getDefaultProps: function() {
+    return { query: "Originals" };
+  },
+  
   render: function() {
-    
+    console.log(this.props.query);
     return (
       <div className='shopApp-container'>
         <div className='shopApp-header'> 
@@ -13,45 +25,27 @@ var ShopApp = React.createClass({
                   <a href="/shop">About</a>
                 </ul>
             </div>
-          <h3> 'CLOTHES CATEGORY NAME' </h3> 
+          <h3> {this.props.query} </h3> 
         </div>
-        <div className='item_container'>
-          <div className='item_desc'> <p> Item Name; $100</p> </div>
-          <img src="http://www.myfamily.it/content/images/thumbs/0024087.jpeg"></img>
-        </div>
-        <div className='item_container'>
-          <div className='item_desc'> <p> Item Name; $100</p> </div>
-          <img src="http://www.myfamily.it/content/images/thumbs/0024087.jpeg"></img>
-        </div>
-        <div className='item_container'>
-          <div className='item_desc'> <p> Item Name; $100</p> </div>
-          <img src="http://www.myfamily.it/content/images/thumbs/0024087.jpeg"></img>
-        </div>
-        <div className='item_container'>
-          <div className='item_desc'> <p> Item Name; $100</p> </div>
-          <img src="http://www.myfamily.it/content/images/thumbs/0024087.jpeg"></img>
-        </div>
-        <div className='item_container'>
-          <div className='item_desc'> <p> Item Name; $100</p> </div>
-          <img src="http://www.myfamily.it/content/images/thumbs/0024087.jpeg"></img>
-        </div>
-        <div className='item_container'>
-          <div className='item_desc'> <p> Item Name; $100</p> </div>
-          <img src="http://www.myfamily.it/content/images/thumbs/0024087.jpeg"></img>
-        </div>
-        <div className='item_container'>
-          <div className='item_desc'> <p> Item Name; $100</p> </div>
-          <img src="http://www.myfamily.it/content/images/thumbs/0024087.jpeg"></img>
-        </div>
-        <div className='item_container'>
-          <div className='item_desc'> <p> Item Name; $100</p> </div>
-          <img src="http://www.myfamily.it/content/images/thumbs/0024087.jpeg"></img>
-        </div>
-        <div className='item_container'>
-          <div className='item_desc'> <p> Item Name; $100</p> </div>
-          <img src="http://www.myfamily.it/content/images/thumbs/0024087.jpeg"></img>
-        </div>
+        <ul className="items_list">
+          {this.state.items.map(function (item) {
+            return (<li key={item.id}> <ItemBox id={item.id} /> </li>);
+          }) }
+        </ul>
+        
       </div>
+    );
+  }
+});
+
+var ItemBox = React.createClass({
+  
+  render: function() {
+    return (
+    <div className='item_container'>
+      <div className='item_desc'> <p> Item Name {this.props.id}; $100</p> </div>
+      <img src="http://www.myfamily.it/content/images/thumbs/0024087.jpeg"></img>
+    </div>
     );
   }
 });
